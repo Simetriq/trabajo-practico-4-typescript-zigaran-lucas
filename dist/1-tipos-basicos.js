311626868003e3ba46e721be1d780b691d1041d7
NatZigaran. <natanaelzigaran@gmail.com>
2025-11-27 15:18:15 -0300

    se volvio a configurar el ts config para el proyecto

---
+++ dist/1-tipos-basicos.js
@@ -0,0 +1,9 @@
+"use strict";
+let productName = "Laptop";
+let productPrice = 1000;
+let productId = "ABC123";
+let myProduct = {
+    name: productName,
+    price: productPrice
+};
+productId = 123;

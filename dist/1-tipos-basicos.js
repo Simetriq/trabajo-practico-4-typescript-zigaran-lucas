e694a3dcd3a24082849ade31b68c1da8ab87161b
NatZigaran. <natanaelzigaran@gmail.com>
2025-11-27 16:26:36 -0300

    se borro el node_modules del repositorio por ser mala practica subirlo al repositorio

--- dist/1-tipos-basicos.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let productName = "Laptop";
-let productPrice = 1000;
-let productId = "ABC123";
-let myProduct = {
-    name: productName,
-    price: productPrice
-};
-productId = 123;
